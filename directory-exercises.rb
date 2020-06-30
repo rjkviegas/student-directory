67a9e0d92643926f919c1d5d8c9b4e9a71e703bb
Ryan <rjkviegas@gmail.com>
2020-06-30 11:58:35 +0000

    Updated directory-exercises.rb to fulfill exercise 8.6 (research and use the String class center() method)

--- directory-exercises.rb
+++ directory-exercises.rb
@@ -1,24 +1,28 @@
+def center(string)
+  puts string.center(100)
+end
+
 def input_students_information
-  puts "Please enter the names of the students to be added to this directory"
-  puts "To finish adding entries, hit 'return' when prompted"
+  center "Please enter the names of the students to be added to this directory"
+  center "To finish adding entries, hit 'return' when prompted"
   # create empty arrays for student information
   students = []
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
-    puts "Please enter #{name}'s country of birth:"
+    center "Please enter #{name}'s country of birth:"
     # get the students country of birth
     country = gets.chomp
-    puts "#{name}'s country of birth is #{country}."
-    puts "Please enter #{name}'s favourite hobby:"
+    center "#{name}'s country of birth is #{country}."
+    center "Please enter #{name}'s favourite hobby:"
     # get the students hobby
     hobby = gets.chomp
     # add students name and information to student array within a hash
     students << {name: name, info: [:november, country, hobby]}
     # inform user of how many students have been inputted
-    puts "Now we have #{students.count} students."
-    puts "Please enter a new student's name or 'return' to finish adding new students and their information"
+    center "Now we have #{students.count} students."
+    center "Enter a new student's name or 'return' to finish adding students and their information"
     # gets another name from the user or break out of while loop
     name = gets.chomp
   end
@@ -27,14 +31,14 @@ def input_students_information
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  center "The students of Villains Academy"
+  center "-------------"
 end
 
 def print(students)
-  puts "Please input the letter you want all the stuent names to be listed to begin with: "
-  puts "For ALL names to be listed, hit 'return'"
-  puts "(Please note only student names shorter than 12 character long will be printed)"
+  center "Please input the letter you want all the stuent names to be listed to begin with: "
+  center "For ALL names to be listed, hit 'return'"
+  center "(Please note only student names shorter than 12 character long will be printed)"
   # get a letter
   letter = gets.chomp
   # conditional for whether a letter is entered or not
@@ -43,7 +47,7 @@ def print(students)
     i = 0
     while students.length > i do
       if students[i][:name].length < 12
-        puts "#{i+1}. #{students[i][:name]} (#{students[i][:info][0]} cohort)"
+        center "#{i+1}. #{students[i][:name]} (#{students[i][:info][0]} cohort)"
         i += 1
       else
         i += 1
@@ -55,7 +59,7 @@ def print(students)
     i = 0
     while students.length > i do
       if students[i][:name][0] == letter && students[i][:name].length < 12
-        puts "#{i+1}. #{students[i][:name]} (#{students[i][:info][0]} cohort)"
+        center "#{i+1}. #{students[i][:name]} (#{students[i][:info][0]} cohort)"
         i += 1
       else
         i += 1 
@@ -65,7 +69,7 @@ def print(students)
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students)."
+  center "Overall, we have #{students.count} great students)."
 end
 
 students = input_students_information
@@ -74,5 +78,5 @@ print_header
 print(students)
 print_footer(students)
 
-puts students
+# puts students
 
